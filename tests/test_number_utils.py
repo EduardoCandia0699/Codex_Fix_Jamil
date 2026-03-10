41cfc588520cc74b05ab3f245732837b1ec3affd
EduardoCandia0699 <eduardo.candia@enroutesystems.com>
2026-03-10 17:58:27 -0600

    First push

---
+++ tests/test_number_utils.py
@@ -0,0 +1,21 @@
+from src.number_utils import number_is_even, sum_of_numbers, number_divisible_by_5
+
+
+def test_number_is_even():
+    assert number_is_even(10)
+
+
+def test_number_is_odd():
+    assert not number_is_even(9)
+
+
+def test_sum_of_numbers():
+    assert sum_of_numbers(9, 10) == 19
+
+
+def test_number_divisible_by_5():
+    assert number_divisible_by_5(50)
+
+
+def test_number_not_divisible_by_5():
+    assert number_divisible_by_5(44)
