2b52ff857ef12425bdaf527c81c74b2a7490b78a
EduardoCandia0699 <eduardo.candia@enroutesystems.com>
2026-03-10 18:44:50 -0600

    Fix everything

--- src/number_utils.py
+++ src/number_utils.py
@@ -2,21 +2,18 @@ def number_is_even(n: int) -> bool:
     """
     Verify the number is an even number
     """
-    # BUG: returns True if the number is odd
-    return n % 2 != 0
+    return n % 2 == 0
 
 
 def sum_of_numbers(n: int, m: int) -> int:
     """
     Sums the numbers
     """
-    # BUG: incorrect result
-    return n - m
+    return n + m
 
 
 def number_divisible_by_5(n: int) -> bool:
     """
     Verify that the number is divisible by 5
     """
-    # BUG: checks that number is divisible by 3 instead of 5
-    return n % 3 == 0
+    return n % 5 == 0
